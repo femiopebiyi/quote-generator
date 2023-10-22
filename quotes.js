68a8b7b4efcb5562cfe65574341391849591ef01
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-10-22 23:04:39 +0100

    added images

--- quotes.js
+++ quotes.js
@@ -2,210 +2,264 @@ export const motivationalQuotes = [
 {
     quote: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
     author: "Winston Churchill",
+    Image: 'images/churchill pic.webp'
 },
 {
     quote: "The only way to do great work is to love what you do.",
     author: "Steve Jobs",
+    Image : 'images/stevejobspic.jpg'
 },
 {
     quote: "Believe you can, and you're halfway there.",
     author: "Theodore Roosevelt",
+    Image: 'images/roosevelt pic.webp'
 },
 {
     quote: "The future belongs to those who believe in the beauty of their dreams.",
     author: "Eleanor Roosevelt",
+    Image : 'images/eleanorpic.webp'
 },
 {
     quote: "Your time is limited, don't waste it living someone else's life.",
     author: "Steve Jobs",
+    Image : 'images/stevejobspic.jpg'
 },
 {
     quote: "In the middle of every difficulty lies opportunity.",
     author: "Albert Einstein",
+    Image : 'images/einsteinpic.jpg'
 },
 {
     quote: "You are never too old to set another goal or to dream a new dream.",
     author: "C.S. Lewis",
+    Image : 'images/lewispic.jpg'
 },
 {
     quote: "The only limit to our realization of tomorrow will be our doubts of today.",
     author: "Franklin D. Roosevelt",
+    Image : 'images/roosevelt pic.webp'
 },
 {
     quote: "The journey of a thousand miles begins with one step.",
     author: "Lao Tzu",
+    Image: 'images/tzupic.jpg'
 },
 {
     quote: "Don't watch the clock; do what it does. Keep going.",
     author: "Sam Levenson",
+    Image : 'images/sampic.jpg'
 },
 {
     quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
     author: "Albert Schweitzer",
+    Image : 'images/albertpic.jpg'
 },
 {
     quote: "Hardships often prepare ordinary people for an extraordinary destiny.",
     author: "C.S. Lewis",
+    Image : 'images/lewispic.jpg'
 },
 {
     quote: "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
     author: "Christian D. Larson",
+    Image : 'images/christianpic.jpg'
 },
 {
     quote: "The only person you are destined to become is the person you decide to be.",
     author: "Ralph Waldo Emerson",
+    Image: 'images/ralphpic.jpg'
 },
 {
     quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
     author: "Martin Luther King Jr.",
+    Image : 'images/kingpic.jpg'
 },
 {
     quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
     author: "Chinese Proverb",
+    Image: 'images/chinesepic.jpg'
 },
 {
     quote: "Don't be afraid to give up the good to go for the great.",
     author: "John D. Rockefeller",
+    Image : 'images/rockefellerpic.webp'
 },
 {
     quote: "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
     author: "Ralph Waldo Emerson",
+    Image: 'images/ralphpic.jpg'
 },
 {
     quote: "The future depends on what you do today.",
     author: "Mahatma Gandhi",
+    Image: 'images/gandhipic.jpg'
 },
 {
     quote: "Success is walking from failure to failure with no loss of enthusiasm.",
     author: "Winston Churchill",
+    Image: 'images/churchill pic.webp'
 },
 {
     quote: "You miss 100% of the shots you don't take.",
     author: "Wayne Gretzky",
+    Image : 'images/waynepic.jpg'
 },
 {
     quote: "The only place where success comes before work is in the dictionary.",
     author: "Vidal Sassoon",
+    Image: 'images/vidalpic.webp'
 },
 {
     quote: "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
     author: "Jordan Belfort",
+    Image: 'images/jordanpic.jpg'
 },
 {
     quote: "You are the master of your destiny. You can influence, direct and control your own environment. You can make your life what you want it to be.",
     author: "Napoleon Hill",
+    Image: 'images/napoleonpic.webp'
 },
 {
     quote: "The road to success and the road to failure are almost exactly the same.",
     author: "Colin R. Davis",
+    Image: 'images/colinpic.jpg'
 },
 {
     quote: "The best revenge is massive success.",
     author: "Frank Sinatra",
+    Image: 'images/sinatrapic.jpg'
 },
 {
     quote: "If you are not willing to risk the usual, you will have to settle for the ordinary.",
     author: "Jim Rohn",
+    Image: 'images/jimpic.jpg'
 },
 {
     quote: "The only person you should try to be better than is the person you were yesterday.",
     author: "Anonymous",
+    Image: 'images/anonpic.png'
 },
 {
     quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
     author: "Roy T. Bennett",
+    Image: 'images/roypic.jpg'
 },
 {
     quote: "The only limit to our realization of tomorrow will be our doubts of today.",
     author: "Franklin D. Roosevelt",
+    Image : 'images/roosevelt pic.webp'
+
 },
 {
     quote: "The journey of a thousand miles begins with one step.",
     author: "Lao Tzu",
+    Image: 'images/tzupic.jpg'
 },
 {
     quote: "Don't watch the clock; do what it does. Keep going.",
     author: "Sam Levenson",
+    Image : 'images/sampic.jpg'
 },
 {
     quote: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
     author: "Albert Schweitzer",
+    Image : 'images/albertpic.jpg'
 },
 {
     quote: "Hardships often prepare ordinary people for an extraordinary destiny.",
     author: "C.S. Lewis",
+    Image : 'images/lewispic.jpg'
+
 },
 {
     quote: "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
     author: "Christian D. Larson",
+    Image : 'images/christianpic.jpg'
 },
 {
     quote: "The only person you are destined to become is the person you decide to be.",
     author: "Ralph Waldo Emerson",
+    Image: 'images/ralphpic.jpg'
 },
 {
     quote: "In the end, we will remember not the words of our enemies, but the silence of our friends.",
     author: "Martin Luther King Jr.",
+    Image : 'images/kingpic.jpg'
 },
 {
     quote: "The best time to plant a tree was 20 years ago. The second best time is now.",
     author: "Chinese Proverb",
+    Image: 'images/chinesepic.jpg'
 },
 {
     quote: "Don't be afraid to give up the good to go for the great.",
     author: "John D. Rockefeller",
+    Image : 'images/rockefellerpic.webp'
 },
 {
     quote: "What lies behind us and what lies before us are tiny matters compared to what lies within us.",
     author: "Ralph Waldo Emerson",
-},
-{
-    quote: "The future depends on what you do today.",
-    author: "Mahatma Gandhi",
-},
-{
-    quote: "Success is walking from failure to failure with no loss of enthusiasm.",
-    author: "Winston Churchill",
-},
-{
-    quote: "You miss 100% of the shots you don't take.",
-    author: "Wayne Gretzky",
-},
-{
-    quote: "The only place where success comes before work is in the dictionary.",
-    author: "Vidal Sassoon",
-},
-{
-    quote: "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
-    author: "Jordan Belfort",
-},
-{
-    quote: "You are the master of your destiny. You can influence, direct and control your own environment. You can make your life what you want it to be.",
-    author: "Napoleon Hill",
-},
-{
-    quote: "The road to success and the road to failure are almost exactly the same.",
-    author: "Colin R. Davis",
-},
-{
-    quote: "The best revenge is massive success.",
-    author: "Frank Sinatra",
-},
-{
-    quote: "If you are not willing to risk the usual, you will have to settle for the ordinary.",
-    author: "Jim Rohn",
-},
-{
-    quote: "The only person you should try to be better than is the person you were yesterday.",
-    author: "Anonymous",
-},
-{
-    quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
-    author: "Roy T. Bennett",
+    Image: 'images/ralphpic.jpg'
 },
 {
     quote: "It's in your hands",
-    author: "Opebiyi Oluwafemi Babatunde"
+    author: "Opebiyi Oluwafemi Babatunde",
+    Image: 'images/femipic.png'
+},
+{
+    quote: "The future depends on what you do today.",
+    author: "Mahatma Gandhi",
+    Image: 'images/gandhipic.jpg'
+},
+{
+    quote: "Success is walking from failure to failure with no loss of enthusiasm.",
+    author: "Winston Churchill",
+    Image: 'images/churchill pic.webp'
+},
+{
+    quote: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+    Image : 'images/waynepic.jpg'
+},
+{
+    quote: "The only place where success comes before work is in the dictionary.",
+    author: "Vidal Sassoon",
+    Image: 'images/vidalpic.webp'
+},
+{
+    quote: "The only thing standing between you and your goal is the story you keep telling yourself as to why you can't achieve it.",
+    author: "Jordan Belfort",
+    Image: 'images/jordanpic.jpg'
+},
+{
+    quote: "You are the master of your destiny. You can influence, direct and control your own environment. You can make your life what you want it to be.",
+    author: "Napoleon Hill",
+    Image: 'images/napoleonpic.webp'
+},
+{
+    quote: "The road to success and the road to failure are almost exactly the same.",
+    author: "Colin R. Davis",
+    Image: 'images/colinpic.jpg'
+},
+{
+    quote: "The best revenge is massive success.",
+    author: "Frank Sinatra",
+    Image: 'images/sinatrapic.jpg'
+},
+{
+    quote: "If you are not willing to risk the usual, you will have to settle for the ordinary.",
+    author: "Jim Rohn",
+    Image: 'images/jimpic.jpg'
+},
+{
+    quote: "The only person you should try to be better than is the person you were yesterday.",
+    author: "Anonymous",
+    Image: 'images/anonpic.png'
+},
+{
+    quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart.",
+    author: "Roy T. Bennett",
+    Image: 'images/roypic.jpg'
 }
 ];
 
--- script.js
+++ script.js
@@ -2,18 +2,25 @@ import { motivationalQuotes } from "./quotes.js";
 
 let quoteDisplay =  document.querySelector('.quote');
 let authorDisplay =  document.querySelector('.author');
+let imageDisplay = document.querySelector('.image')
 const autoGenerate = document.querySelector('.auto');
 const generateButton  = document.querySelector('.generate');
 
 
+
+
 function generateRandomQuotes (){
-    let randomizer = Math.floor(Math.random()*motivationalQuotes.length-1)
+    let randomizer = Math.floor(Math.random()*(motivationalQuotes.length-1))
 
     let randomQuote = motivationalQuotes[randomizer].quote
     let randomAuthor = motivationalQuotes[randomizer].author
+    let randomImage = motivationalQuotes[randomizer].Image
+    console.log(randomImage)
 
     quoteDisplay.innerHTML = `❝${randomQuote}❞`
     authorDisplay.innerHTML = randomAuthor
+    imageDisplay.innerHTML = `<img src="./${randomImage}" alt="">`
+
 }
 
 generateRandomQuotes()
@@ -33,7 +40,6 @@ autoGenerate.addEventListener ('click', () => {
     }, 2000)
     autoGenerate.innerHTML = 'Stop'
     isAuto = true
-    console.log(intervalId)
     } else{
         clearInterval(intervalId)
         isAuto =false
