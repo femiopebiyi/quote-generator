afe29615b0531a5cd9840bdd6b0e1c3fadba457c
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-10-22 21:02:11 +0100

    auto generate button

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ import { motivationalQuotes } from "./quotes.js";
 
 let quoteDisplay =  document.querySelector('.quote');
 let authorDisplay =  document.querySelector('.author');
-const generateButton  = document.querySelector('.generate')
+const autoGenerate = document.querySelector('.auto');
+const generateButton  = document.querySelector('.generate');
 
 
 function generateRandomQuotes (){
     let randomizer = Math.floor(Math.random()*motivationalQuotes.length-1)
+
     let randomQuote = motivationalQuotes[randomizer].quote
     let randomAuthor = motivationalQuotes[randomizer].author
 
@@ -18,4 +20,24 @@ generateRandomQuotes()
 
 generateButton.addEventListener ('click', function(){
     generateRandomQuotes()
+})
+
+let isAuto = false
+let intervalId;
+
+autoGenerate.addEventListener ('click', () => {
+    
+    if(isAuto === false){
+        intervalId = setInterval(function(){
+        generateRandomQuotes()
+    }, 2000)
+    autoGenerate.innerHTML = 'Stop'
+    isAuto = true
+    console.log(intervalId)
+    } else{
+        clearInterval(intervalId)
+        isAuto =false
+        autoGenerate.innerHTML = 'Auto Generate'
+    }
+
 })
